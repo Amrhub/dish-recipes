90b59c19833a1ee9e5c061c878e88be48a7485ae
Amrhub <amr_ahmed65@hotmail.com>
2022-03-08 23:14:38 +0200

    Setup linters configuration

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,10 +1,10 @@
 class UserNotifierMailer < ApplicationMailer
-  default :from => 'dishrecipe@awesome.io'
+  default from: 'dishrecipe@awesome.io'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
     @user = user
-    mail( :to => @user.email,
-    :subject => 'Thanks for signing up for our amazing app' )
+    mail(to: @user.email,
+         subject: 'Thanks for signing up for our amazing app')
   end
 end
