55d897dd508ddaba227d6b66080ee98f553476fd
Leonardoeng13 <leonardoandrade.eng@gmail.com>
2022-03-08 19:01:52 -0300

    Sendgrid functions

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,5 +1,5 @@
 class UserNotifierMailer < ApplicationMailer
-  default from: 'dishrecipe@awesome.io'
+  default from: 'leonardoandrade.eng@gmail.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
