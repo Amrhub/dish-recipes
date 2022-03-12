0642af783ca3c9801cbe6a3933b2004de4786481
Amrhub <amr_ahmed65@hotmail.com>
2022-03-12 08:58:25 +0200

    display current user on navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  get 'shopping_list/index'
   devise_for :users
 
   resources :foods , :except => [:show]
-  resources :inventories do 
-    resources :inventory_foods
-  end
   resources :recipes do 
     resources :recipe_foods
     get '/general_shopping_list', to: 'shopping_list#index', as:  'shopping_list'
   end
 
   root "recipes#index"
+
+  # future work
+    # resources :inventories do 
+    #   resources :inventory_foods
+    # end
 end
