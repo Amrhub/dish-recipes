17db61d02680c83462d28f582f0d54f1d9a49f08
Amrhub <amr_ahmed65@hotmail.com>
2022-03-12 18:42:02 +0200

    Add default url options

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+Rails.application.routes.default_url_options[:host] = "gitrecipe.herokuapp.com"
 
 ActionMailer::Base.smtp_settings = {
     user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
