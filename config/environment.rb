abb746552a62e30055e4f972af4c30df95c4ab82
Amrhub <amr_ahmed65@hotmail.com>
2022-03-14 07:46:37 +0200

    Adding rememberable + allow user to sign in

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,3 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-Rails.application.routes.default_url_options[:host] = "gitrecipe.herokuapp.com"
-
-ActionMailer::Base.smtp_settings = {
-    user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-    password: ENV["sendgrid_password"], # This is the secret sendgrid API key which was issued during API key creation
-    domain: 'gitrecipe.herokuapp.com',
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
